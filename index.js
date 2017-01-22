0df1a9fee03ee85d89b46ec068ff5189e1a1c3b6
Deep Desai <ddesai@ca.ibm.com>
2017-01-21 23:08:15 -0500

    parsed the score

--- index.js
+++ index.js
@@ -8,14 +8,32 @@ var port = 30015;
 
 
 
-app.get('/',function(err, res){
+// app.get('/',function(err, res){
 
-var url = 'http://www.espncricinfo.com/new-zealand-v-bangladesh-2016-17/engine/match/1019987.html';
+var url = 'http://www.espncricinfo.com/ci/engine/match/index.html';
 request(url, function(err, response, html){
-  res.send(html);
+  //res.send(html);
+var $ = cheerio.load(html);
+
+// var text = $('.innings-1-score', '.team-1-name', '.large-13 medium-13 columns innings-information','.row brief-summary').text();
+var data = $('div.innings-info-1, div.innings-info-2').filter(function(){
+
+return $(this).text().trim().includes('Australia');
+
+
 
 });
+
+var team1score = data.text().split(" ")[4] + data.text().split(" ")[5] + " overs)";
+
+// res.send(html);
+console.log(team1score);
+console.log(data.next().text());
+console.log("Heellooo");
 });
+// });
+
+
 
 app.listen(port);
 console.log("Port set to " + port);
